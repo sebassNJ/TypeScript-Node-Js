01b9223160da07429b3a63db312b74888e20d502
sebas <sebassjuan2389@gmail.com>
2025-11-25 22:00:17 -0500

    cuarto commit sin node_module

--- dist/app.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const heroes_services_1 = require("./services/heroes.services");
-const personaje = (0, heroes_services_1.usuario)(1);
-console.log("hello word");
-console.log(personaje);
--- dist/services/heroes.services.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.usuario = void 0;
-const superHeroes_1 = require("../data/superHeroes");
-const usuario = (id) => {
-    return superHeroes_1.heroes.find((hero) => hero.id === id);
-};
-exports.usuario = usuario;
